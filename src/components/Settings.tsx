35ed961061203cfc737286bb2d85db14555b58b7
Jeffrey Wardman <23271678+JeffreyWardman@users.noreply.github.com>
2026-04-17 23:48:27 +1000

    revert versioning

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,7 +5,7 @@ import { applyTheme, useTheme } from "../ThemeContext";
 import type { PaneLayout, Profile } from "../types";
 import { defaultShell, formatCwd, noAutocorrect, pathBasename } from "../utils";
 
-const APP_VERSION = "0.1.0";
+const APP_VERSION = "0.0.0";
 
 const TILING_OPTIONS: PaneLayout[] = [
 	"1x1",
